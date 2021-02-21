f0015d742508b7019f8d1039e118247675cd33b3
Muhammad Hasan <muh.hasan.git@gmail.com>
2021-02-21 15:05:12 +0500

    error page done

--- src/pages/301.js
+++ src/pages/301.js
@@ -1,11 +1,12 @@
 import React from "react"
 import { Link } from "@reach/router"
+import Button from "../components/Button"
 const Error = () => {
   return (
     <div className="text-center">
       <h2>Page not found</h2>
       <Link to="/">
-        <button>Go to Home</button>
+        <Button className="btn-purple" text="Go To home" />
       </Link>
     </div>
   )
